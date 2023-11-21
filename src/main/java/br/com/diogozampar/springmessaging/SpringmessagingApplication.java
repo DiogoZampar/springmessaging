cb729a29cb3decba06845efc1ebdfb17aacd959d
DiogoZampar <diogozampar@gmail.com>
2023-11-20 23:35:41 -0300

    first commit

---
+++ src/main/java/br/com/diogozampar/springmessaging/SpringmessagingApplication.java
@@ -0,0 +1,13 @@
+package br.com.diogozampar.springmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diogozampar/springmessaging/SpringmessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diogozampar.springmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
